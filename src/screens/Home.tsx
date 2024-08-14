b144d69354e8d82f0a0ae2c1624e51f02827e4a1
Jose Carvajal <jcarvajal373@gmail.com>
2024-08-14 11:21:55 -0400

    feat: adding view controls

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,22 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import {TypoBase} from '../components/typography/TypoBase';
 import {BaseLayout} from '../components/layout/BaseLayout';
 import {Header} from '../components/header/Header';
 import {Button} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import {Controls} from '../components/controls/Controls';
+import {viewTypes} from '../models/common';
 export const Home: FC = () => {
   const {navigate} = useNavigation();
+  const [viewMode, setViewMode] = useState<viewTypes>('day');
+  const onChangeViewMode = (value: viewTypes) => setViewMode(value);
   return (
     <BaseLayout>
       <Header title="Home" />
+      <Controls
+        currentViewMode={viewMode}
+        onChangeViewMode={onChangeViewMode}
+      />
       <TypoBase fontStyle="bold" size="headline">
         {'Hello'}
       </TypoBase>
